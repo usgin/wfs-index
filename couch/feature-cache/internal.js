82db3c85897f83663fe6f0fbbc29dbd775b5e8b6
Ryan Clark <ryan.clark.j@gmail.com>
2013-08-30 16:39:23 -0700

    add a list that can be used to retrieve a FeatureCollection

--- couch/feature-cache/internal.js
+++ couch/feature-cache/internal.js
@@ -2,8 +2,26 @@ var byWfsDocId = function (doc) {
     emit(doc.wfsDocId, 1);
 };
 
+var data = function (doc) {
+    emit(doc.wfsDocId, doc);    
+};
+
 var byWfsDocIdReduce = function (keys, values) {
     return sum(values);
+};
+
+var featureCollection = function (head, req) {
+    send('{"type": "FeatureCollection", "features": [');
+
+    var row = getRow();
+    
+    if (row) { send(JSON.stringify(row.value)); }
+
+    while (row = getRow()) {
+        send(', ' + JSON.stringify(row.value));
+    }
+    
+    send(']}');
 };
 
 module.exports = {
@@ -15,6 +33,14 @@ module.exports = {
         byWfsDocId: {
             map: byWfsDocId.toString(),
             reduce: byWfsDocIdReduce.toString()
+        },
+        
+        data: {
+            map: data.toString()    
         }
+    },
+    
+    lists: {
+        featureCollection: featureCollection.toString()
     }
 };
