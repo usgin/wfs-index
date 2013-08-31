dfa95c6c434a7f77234fb9055095c63f94410c61
Ryan Clark <ryan.clark.j@gmail.com>
2013-08-30 17:46:32 -0700

    find map functions dynamically

--- maps/index.js
+++ maps/index.js
@@ -1,3 +1,17 @@
-module.exports = {
-    thermalSprings: require('./thermalSprings').toString()
-};
+var fs = require('fs'),
+    path = require('path'),
+    
+    _ = require('underscore'),
+    
+    here = path.dirname(__filename),
+    files = fs.readdirSync(here),
+    pairs = [];
+
+_.each(files, function (filename) {
+    if (__filename.indexOf(filename) === -1) {
+        var key = filename.replace('.js', '');
+        pairs.push([key, require('./' + key).toString()]);
+    }
+});
+
+module.exports = _.object(pairs);
