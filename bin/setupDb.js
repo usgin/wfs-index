e9bf24758d76abc275de325d6b78aef55d26a55e
Ryan Clark <ryan.clark.j@gmail.com>
2013-09-06 16:08:18 -0700

    remove some cruft

--- bin/setupDb.js
+++ bin/setupDb.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 var couch = require('../couch');
 
-couch.makeDbs(function (err) {
+couch.setup(function (err) {
     if (err) { console.log(err); return; }
-    couch.setup(function (err) {
-        if (err) { console.log(err); return; }
-        console.log('Databases have been set up');
-    });
-})
+    console.log('Databases have been set up');
+});
--- indexer/addToIndex.js
+++ indexer/addToIndex.js
@@ -20,7 +20,3 @@ module.exports = function addToIndex(mapName, callback) {
         client.add(docs, callback);
     });
 };
-
-//'{"_southWest":{"lat":37.41925395973696,"lng":-92.51861572265625},"_northEast":{"lat":37.72076290898373,"lng":-91.1480712890625}}'
-
-//geo:[37.41925395973696,-92.51861572265625 TO 37.72076290898373,-91.1480712890625]
